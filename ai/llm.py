23d626332df25e1d2300a8c02ea3872814b2c1d2
Ian Clarke <ianclarke@uvic.ca>
2026-02-14 22:01:42 -0800

    Improved llm parse prompting

--- ai/llm.py
+++ ai/llm.py
@@ -2,29 +2,81 @@ import requests
 import time
 
 def get_dbs(message):
+    prompt1 = f"""
+Extract the number of dbs.
+End your output with ###END###
+If no items have prices in dbs or you cannot parse correctly, output Fail###END###.
+Otherwise output exactly one line per item in this format:
+[Item] Output: <number of dbs for that item>
+
+[a][b]4dbs/5dbs
+[a] 4
+[b] 5###END###
+
+[a]1db
+[1] Output: 1###END###
+
+[a][b]1db each
+[a] 1
+[b] 1###END###
+
+[a]
+Fail###END###
+
+[a]pls give me free stuff
+Fail###END###
+
+Message: {message}
+"""
     prompt = f"""
-    Extract the number of dbs in the last message. 
-    If none is listed, output 0.
-    Output exactly one line per item in this format:
-    [Item] Output: <number>
-    End your output with ###END###
+You are a strict parser for item db counts. Follow these rules exactly:
 
-    Message: [a][b]4dbs/5dbs
-    [a] Output: 4
-    [b] Output: 5###END###
+1. Only consider numeric values immediately followed by "db" or "dbs".  
+2. Ignore all other numbers, words, URLs, or offers.  
+3. For each item ([a], [b], [c], etc.), output exactly one line in this format:
+[Item] Output: <number of dbs>
+4. If an item is listed but no valid db number exists, output nothing for it.
+5. If no items have valid db numbers, output exactly:
+Fail###END###
+6. End your output with ###END###.
 
-    Message: [a]1db
-    [b] Output: 1###END###
+Examples:
 
-    Message: [a][b]1db each
-    [a] Output: 1
-    [b] Output: 1###END###
+[a][b]4dbs/5dbs
+[a] Output: 4
+[b] Output: 5
+###END###
 
-    Message: [a]pls give me free stuff
-    [a] Output: 0###END###
+[a]1db
+[a] Output: 1
+###END###
 
-    Message: {message}
-    """
+[a][b]1db each
+[a] Output: 1
+[b] Output: 1
+###END###
+
+[a]
+Fail###END###
+
+[a]pls give me free stuff
+Fail###END###
+
+[a]61+2 wts 2db+2full or offer
+[a] Output: 2
+###END###
+
+[a][b][c]4db / 2db / 1db
+[a] Output: 4
+[b] Output: 2
+[c] Output: 1
+###END###
+
+[a]ZIKRON ICE GET FRESH
+Fail###END###
+
+Message: {message}
+"""
 
     start_time = time.time()
 
